2a1944cc7aaebd2227a391670467ae997b1de383
Alan So <aso@mavenlink.com>
2022-07-26 15:59:17 -0700

    Added error handling for paginate_response (if response list is nil)

--- lib/coda_docs/entities/entity.rb
+++ lib/coda_docs/entities/entity.rb
@@ -28,7 +28,8 @@ module CodaDocs
           response_list << parse_response(response)
         end
 
-        response_list.flatten if response_list.present?
+        raise StandardError.new("Could not retrieve entity. Either entity does not exist or you do not have authorization to access the entity.") unless response_list.present?
+        response_list.flatten
       end
     end
   end
